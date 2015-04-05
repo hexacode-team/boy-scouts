661f994161e394cdf06b2d2c69242081585eb5a2
Steven Manning <manning@tamu.edu>
2015-04-05 14:39:39 -0500

    Added Payment model.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,5 @@
 class Subscription < ActiveRecord::Base
+    belongs_to: member
+    belongs_to: route
+    
 end
---
+++ db/migrate/20150405193933_create_payments.rb
@@ -0,0 +1,11 @@
+class CreatePayments < ActiveRecord::Migration
+  def change
+    create_table :payments do |t|
+      t.integer, :subscription_id
+      t.decimal, :amount_paid
+      t.datetime :date
+
+      t.timestamps null: false
+    end
+  end
+end
