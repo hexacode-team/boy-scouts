3fc10de31ee0ff436d2f446f46e837bd2303d212
Steven Manning <manning@tamu.edu>
2015-04-13 15:43:47 -0500

    Removed members model and encorporated member columns into the user model.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ActiveRecord::Base
   belongs_to :group
+  has_many :runs
 
 end
--- db/migrate/20150405183111_create_members_runs.rb
+++ db/migrate/20150405183111_create_members_runs.rb
@@ -1,7 +1,7 @@
 class CreateMembersRuns < ActiveRecord::Migration
   def change
     create_table :members_runs do |t|
-      t.references :member, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.references :run, index: true, foreign_key: true
 
       t.timestamps null: false
