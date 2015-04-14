f7c17d17e943f7d9dd110ecb0fe0a4c00d605fdf
Steven Manning <manning@tamu.edu>
2015-04-14 12:56:55 -0500

    Added controllers, actions, and routes for groups, routes, and run sheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
+  get 'routes/view_routes'
+  get 'routes/view_route'
+
+  get 'group/view_groups'
+  get 'group/view_group'
+
+  get 'run_sheets/generate_runsheet'
+  get 'run_sheets/generate_runsheet_pdf'
+
+  get 'run_sheets/generate_runsheets'
+  get 'run_sheets/generate_runsheets_pdf'
+
+
+  get 'payment/generate_invoice'
+  get 'payment/generate_invoice_pdf'
+
+  get 'payment/generate_invoices'
   get 'payment/generate_invoices_pdf'
 
-  get 'payment/generate_invoices_html'
 
-  get 'run_sheets/generate_runsheets_pdf'
-  get 'run_sheets/generate_runsheets_html'
+
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'welcome/index'
---
+++ spec/controllers/group_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe GroupController, type: :controller do
+
+  describe "GET #view_groups" do
+    it "returns http success" do
+      get :view_groups
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #view_group" do
+    it "returns http success" do
+      get :view_group
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
