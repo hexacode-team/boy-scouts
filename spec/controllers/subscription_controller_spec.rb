32a6c50f848187c616dd757209a04beca86ffabe
Steven Manning <manning@tamu.edu>
2015-04-15 12:05:21 -0500

    Added rspec route tests for invoice generation

--- spec/controllers/subscription_controller_spec.rb
+++ spec/controllers/subscription_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe PaymentController, type: :controller do
+RSpec.describe SubscriptionController, type: :controller do
 
-  describe "GET #generate_invoices_pdf" do
+  describe "GET #generate_invoices" do
     it "returns http success" do
-      get :generate_invoices_pdf
+      get :generate_invoices
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #generate_invoices_html" do
+  describe "GET #generate_invoices_pdf" do
     it "returns http success" do
-      get :generate_invoices
+      get :generate_invoices_pdf
       expect(response).to have_http_status(:success)
     end
   end
