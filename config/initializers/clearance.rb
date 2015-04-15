7a35660bbf976ea8d64100425c3b874d04d681ac
InsanitIgnis <insanit.ignis@gmail.com>
2015-04-15 14:55:50 -0500

    .

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,10 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+    
+      # Specify some defaults for the user authentication.
+      config.user_model = User
+      config.cookie_domain = '.boyscoutsflagroutes.com'
+      config.cookie_name = 'authentication_token'
+    
+      # TODO: Use proper email.
+    config.mailer_sender = "reply@example.com"
 end
