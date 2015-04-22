829a254bcc25276df028ee7ef7e4cdfccceeea1b
EC2 Default User <ec2-user@ip-172-31-27-143.us-west-2.compute.internal>
2015-04-22 18:44:53 +0000

    Generate pdfs using wicked pdfs...this requires the user of inline styling which can be loaded by setting the render options to use :layout static_layout

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -19,11 +19,21 @@ class SubscriptionController < ApplicationController
     @group = Group.find(params[:group_id])
     @user = current_user
     @no_header = true
+  
+    render :layout => "static_layout"
   end
 
   def generate_invoices_pdf
-    kit = PDFKit.new(generate_invoices_pdf_url , :page_size => 'Letter', :orientation=>'Portrait')
-    pdf = kit.to_pdf
-    send_data(pdf, :filename => "consolidated_invoices.pdf",  :type => "application/pdf")
+    @invoices = Subscription.get_require_invoices(params[:group_id]).sort do |a, b|
+      comp = (a.last_name <=> b.last_name)
+      comp.zero? ? (a.first_name <=> b.first_name) : comp
+    end
+
+    @group = Group.find(params[:group_id])
+    @user = current_user
+    @no_header = true
+
+    render pdf: "test", :layout => "static_layout"
+
   end
 end
