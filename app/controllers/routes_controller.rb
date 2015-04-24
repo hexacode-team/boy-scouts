94138effd4cb1360d1d225497784cac5ca00056e
KReg27 <keithregister@Keiths-MacBook-Pro.local>
2015-04-23 22:11:03 -0500

    Fixed Group List

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,5 +3,8 @@ class RoutesController < ApplicationController
   end
 
   def view_route
+  	@routes = Route.get_routes(params[:group_id])
+  	@user = current_user
+  	@group = Group.where(:group_id => params[:group_id])
   end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,14 @@ class Route < ActiveRecord::Base
   has_many :subscriptions
   has_many :runs
 
+  def self.get_routes(group_id)
+  	groups = Group.where(:group_id => group_id)
+  	routes = []
+
+  	groups.each do |group|
+  		routes += Route.where(:group_id => group.id)
+  	end
+
+  end
+
 end
