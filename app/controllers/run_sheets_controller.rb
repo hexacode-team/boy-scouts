dc4c5e9fc52b05fa500dc0325a3c7d82a450d297
Jonathan Willing <jwilling@me.com>
2015-05-03 14:50:44 -0500

    refactoring to remove code duplication

--- app/controllers/run_sheets_controller.rb
+++ app/controllers/run_sheets_controller.rb
@@ -1,82 +1,31 @@
 class RunSheetsController < ApplicationController
 
-  #For generating runsheets by User
-  def generate_runsheet_pdf
-    @user = current_user
-    @group = current_user.get_groups
-    @nameHash = Hash.new
-    @routesHash = Hash.new
-    @routes = []
-    @routes = Route.where(:group_id => @group[0].id)
-    @routes.each do |route|
-        @subscribers = []
-        @subscribers += route.subscriptions
-
-        @nameHash[route.id] = route.name
-          
-        @routesHash[route.id] = @subscribers
-        @routesHash[route.id].sort_by! {|x| x.visit_sequence}
-      end
-
-      render pdf: "runsheets", :layout => "static_layout"
-  end
-
-  def generate_runsheet_for_user
-    @user = current_user
-    @group = current_user.get_groups
-    @nameHash = Hash.new
-    @routesHash = Hash.new
-    @routes = []
-    @routes = Route.where(:group_id => @group[0].id)
-      @routes.each do |route|
-          @subscribers = []
-          @subscribers += route.subscriptions
-
-          @nameHash[route.id] = route.name
-          
-          @routesHash[route.id] = @subscribers
-          @routesHash[route.id].sort_by! {|x| x.visit_sequence}
-      end
-  end
-
-  #For generating runsheets by Group
+  # For generating runsheets by group.
   def generate_runsheets_pdf
-      @user = current_user
-      group_id = params[:id]
-      @group = Group.find(group_id)
-      @nameHash = Hash.new
-      @routesHash = Hash.new
-      @routes = @group.routes
-      @routes.each do |route|
-          @subscribers = []
-          @subscribers += route.subscriptions
-          
-          @nameHash[route.id] = route.name
+    @group = Group.find(params[:id])
+    prepare_runsheets_for_routes(@group.routes)
 
-          @routesHash[route.id] = @subscribers
-          @routesHash[route.id].sort_by! {|x| x.visit_sequence}
-      end
-
-      render pdf: "runsheets", :layout => "static_layout"
+    render pdf: "runsheets", :layout => "static_layout"
   end
 
   def generate_runsheets_for_group
-      @user = current_user
-      group_id = params[:id]
-      @group = Group.find(group_id)
-      @nameHash = Hash.new
-      @routesHash = Hash.new
-      @routes = @group.routes
-      @routes.each do |route|
-          @subscribers = []
-          @subscribers += route.subscriptions
-          
-          @nameHash[route.id] = route.name
-
-          @routesHash[route.id] = @subscribers
-          @routesHash[route.id].sort_by! {|x| x.visit_sequence}
-      end
+    @group = Group.find(params[:id])
+    prepare_runsheets_for_routes(@group.routes)
   end
 
+  def prepare_runsheets_for_routes(routes)
+    @routes = routes
+    @nameHash = Hash.new
+    @routesHash = Hash.new
+    routes.each do |route|
+      @subscribers = []
+      @subscribers += route.subscriptions
+        
+      @nameHash[route.id] = route.name
+
+      @routesHash[route.id] = @subscribers
+      @routesHash[route.id].sort_by! { |x| x.visit_sequence }
+    end
+  end
 
 end
