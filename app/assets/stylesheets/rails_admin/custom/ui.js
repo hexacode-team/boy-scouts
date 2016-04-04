09d675d31840a4e1cfc20136f44e6485597b4305
twojoke <thomas.t.morris@gmail.com>
2016-04-04 17:34:44 -0500

    fixed issues with responsive design on edit pages and list pages

--- app/assets/stylesheets/rails_admin/custom/ui.js
+++ app/assets/stylesheets/rails_admin/custom/ui.js
@@ -90,10 +90,12 @@ function propogateHeaders() {
             var att = document.createAttribute("data-header");
             att.value = header_names[j];
             tds.item(j).setAttributeNode(att);
-            var children = tds.item(j).getElementsByTagName("a");
 
             //Add &nbsp to empty tds to fix styling issue
-            if (tds.item(j).innerHTML == "" || tds.item(j).innerHTML == "\n" || (children.length != 0 && children.item(0).innerText == ""))
+            var item = tds.item(j);
+            var children = item.getElementsByTagName("a");
+            var list = item.getElementsByTagName("ul");
+            if (item.innerHTML == "" || item.innerHTML == "\n" || (children.length != 0 && children.item(0).innerText == "") && list.length == 0)
                 tds.item(j).innerHTML = "&nbsp";
         }
     }
