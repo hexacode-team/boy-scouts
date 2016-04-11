5cfd601702700e5688da10c00098dbe7bd9c30b6
twojoke <thomas.t.morris@gmail.com>
2016-04-11 16:54:47 -0500

    added rspec tests for update_route and modify_marker controller methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'clearance/rspec'
-
+#require 'support/features/clearance_helpers'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
